a5be999af8db1fcbf3b9fe0ad69d3f4810544033
xushuo <893804934@qq.com>
2018-07-22 02:17:57 +0800

    提问  列表

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,16 +1,35 @@
 import {Component} from '@angular/core';
-import {ModalController, NavController} from 'ionic-angular';
+import {
+    LoadingController, ModalController, NavController, NavParams, Tabs, ToastController,
+    ViewController
+} from 'ionic-angular';
+import {Storage} from "@ionic/storage";
 import {QuestionPage} from "../question/question";
+import {RestProvider} from "../../providers/rest/rest";
+import {BaseUI} from "../../common/baseui";
 
 @Component({
     selector: 'page-home',
     templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage extends BaseUI {
+
+    public feeds: string[];
+    public errorMessage: any
 
     constructor(public navCtrl: NavController,
-                public modalCtrl: ModalController) {
+                public viewController: ViewController,
+                public loadingCtrl: LoadingController,
+                public rest: RestProvider,
+                public toastCtrl: ToastController,
+                public modalCtrl: ModalController,
+                public storage: Storage,
+                public navParams: NavParams) {
+        super()
+    }
 
+    ionViewDidLoad(){
+        this.getFeeds()
     }
 
     gotoQuestion() {
@@ -18,4 +37,23 @@ export class HomePage {
         modal.present()
     }
 
+    gotoChat() {
+        this.selectTab(2)
+    }
+
+    //选定制定tab
+    selectTab(index) {
+        var t: Tabs = this.navCtrl.parent;
+        t.select(index)
+    }
+
+    getFeeds() {
+        var loading = super.showLoading(this.loadingCtrl, "加载中...");
+        this.rest.getFeeds().subscribe(
+            e => {
+                this.feeds = e
+                loading.dismiss();
+            },
+            error => this.errorMessage = <any>error);
+    }
 }
